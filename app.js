b9c35d9594b7684b3f8abfcab931d4a8e29923ca
Erick Ruiz de Chavez <erickrdch@gmail.com>
2012-05-31 07:24:23 -0400

    testing process.pid

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.listen(3000, function() {
 	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
+console.log('This process is pid ' + process.pid);
+
 // Add listeners to the sockets
 io.sockets.on('connection', function(socket) {
 
