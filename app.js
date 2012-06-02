8664957e0d34d8d100dcdfb0b06fbc488eca4be2
Erick Ruiz de Chavez <erickrdch@gmail.com>
2012-06-02 18:55:18 -0400

    oops

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.listen(3000, function() {
 	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
-console.log('This process is pid ' + process.pid);
+// Esta linea mata la app (y el server :S)
+// console.log('This process is pid ' + process.pid);
 
 // Add listeners to the sockets
 io.sockets.on('connection', function(socket) {
